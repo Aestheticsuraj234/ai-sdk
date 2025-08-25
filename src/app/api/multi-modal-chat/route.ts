01096b2ebc900bbf8d6b4e8d17f2a3f13cb0556a
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-08-25 15:31:25 +0530

    multi modal chat

---
+++ src/app/api/multi-modal-chat/route.ts
@@ -0,0 +1,21 @@
+import { openai } from "@ai-sdk/openai";
+import { convertToModelMessages, streamText, UIMessage } from "ai";
+
+export async function POST(req:Request) {
+    try {
+         const {messages}:{messages:UIMessage[]} = await req.json();
+
+
+        const result = streamText({
+            model:openai("gpt-4.1-nano"),
+            messages:convertToModelMessages(messages)
+          
+        });
+
+        return result.toUIMessageStreamResponse();
+    } catch (error) {
+        console.error("Error streaming chat completion:", error);
+        return Response.json({ error: "Failed to stream chat completion" }, { status: 500 });
+    }
+       
+}
